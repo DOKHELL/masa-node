2b58206bf0be08870f0079935a6e2b55c2895126
Serhii Hladkov <serhii.hladkov@p2h.com>
2022-08-08 23:54:22 +0300

    init

--- masa.sh
+++ masa.sh
@@ -1,8 +1,9 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install curl tar wget clang ca-certificates gnupg lsb-release pkg-config libssl-dev jq build-essential git make ncdu net-tools -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#sudo apt install apt-transport-https ca-certificates curl software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get install docker
 sudo apt update
 sudo apt install docker-ce -y
 sudo systemctl start docker
