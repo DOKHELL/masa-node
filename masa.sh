6a458dd68e4317b43f440aab331d6f93416d9fa9
Serhii Hladkov <serhii.hladkov@p2h.com>
2022-08-08 23:20:17 +0300

    init

--- masa.sh
+++ masa.sh
@@ -1,10 +1,12 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install curl tar wget clang ca-certificates gnupg lsb-release pkg-config libssl-dev jq build-essential git make ncdu net-tools -y
-sudo mkdir -p /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce -y
+sudo systemctl start docker
+sudo systemctl enable docker
 
 sudo curl -L "https://github.com/docker/compose/releases/download/2.8.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
